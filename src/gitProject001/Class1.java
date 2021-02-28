c36913948fb47d9a7af23f94769f1bcb7eaab81d
Ibrahim Bayram <ibrahim33bayram@gmail.com>
2021-02-28 17:46:15 +0100

    fift commit

--- src/gitProject001/Class1.java
+++ src/gitProject001/Class1.java
@@ -7,6 +7,9 @@ public class Class1 {
 		//Welcome to Git Sessions
 		// Thank you for joining
 		//here is a new commit from virtual
+		// it is done on PC , hope to see changes on virtual
+		
+		
 		
 
 	}
