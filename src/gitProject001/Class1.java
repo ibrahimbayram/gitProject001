093a5781923208348ed5591e3057ebd81378a487
ibrahimbayram <79757271+ibrahimbayram@users.noreply.github.com>
2021-02-28 17:48:22 +0100

    Update Class1.java

--- src/gitProject001/Class1.java
+++ src/gitProject001/Class1.java
@@ -8,6 +8,9 @@ public class Class1 {
 		// Thank you for joining
 		//here is a new commit from virtual
 		// it is done on PC , hope to see changes on virtual
+		// Changes from local are welcome to virtual. local will accept from the virtual.
+		// I think this will make me to talk myself during Corona :)
+		
 		
 		
 		
